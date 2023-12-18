1c9723936c9b14b17e95b6c520d20b62371f8861
Nathan-88 <onwukaebuka88@gmail.com>
2023-12-18 13:32:08 +0100

    layout design

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="bg-white border-none shadow-md rounded-lg mx-auto mr-6">
+    <div class="flex items-center justify-between px-4 py-2">
+      <div class="flex items-center relative">
+        <input type="text" class="rounded-lg shadow-lg w-32 md:w-52 lg:w-72 h-10 placeholder:text-start placeholder:pr-6 placeholder:text-xl focus:outline-none p-2" placeholder="search">
+        <span class="fas fa-search absolute right-0 pl-2s text-gray-400 text-xl ml-2"></span>
+      </div>
+
+      <div class="flex items-center h-[3rem]">
+        <img src="/assets/images/Frame.png" alt="User Avatar" class="rounded-full">
+        <span class="text-sm font-bold">Nathan</span>
+      </div>
+
+      <div class="pr-4">
+        <span class="fas fa-bell"></span>
+      </div>
+
+          
+    </div>
+  </nav>
+</template>
+
+<script setup>
+// You can add any script logic or data setup here if needed
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@pinia/nuxt','@nuxtjs/tailwindcss',]
+  devtools: { enabled: false },
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@pinia/nuxt']
 })
