6cfd65c33a822898667e864fe254bd40cf26284e
Dev-wonderful <adeonederful20@gmail.com>
2024-01-04 17:49:11 +0100

    mod

--- server/api/v1/schedule.get.ts
+++ server/api/v1/schedule.get.ts
@@ -1,11 +1,23 @@
 import type { ScheduleType } from "~/types"
 import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 export default defineEventHandler(async (event) => {
     // created to test store and state management and also to mock a network call
     const query = getQuery(event)
     
     const month = typeof query.month === 'string' ? query.month : null
+
+    const fileExists = fs.existsSync('data.json');
+    console.log(fileExists);
+    console.log(import.meta.url)
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.join(path.dirname(__filename));
+    const __dirname_2 = path.join(path.dirname(__filename), '../..');
+    console.log(__dirname)
+    console.log(__dirname_2)
+    // if (!fileExists) fs.writeFileSync('/data.json', JSON.stringify({}))
     
     const response = fs.readFileSync('data.json', { encoding: 'utf-8' })
     // console.log('sync file', JSON.parse(response))
