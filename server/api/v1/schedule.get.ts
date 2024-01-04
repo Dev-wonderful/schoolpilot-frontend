36270097f93fc74dc4f4d0381f9530f67abc9d86
Dev-wonderful <adeonederful20@gmail.com>
2024-01-04 23:47:26 +0100

    mod

--- server/api/v1/schedule.get.ts
+++ server/api/v1/schedule.get.ts
@@ -2,6 +2,7 @@ import type { ScheduleType } from "~/types"
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import process from 'process'
 
 export default defineEventHandler(async (event) => {
     // created to test store and state management and also to mock a network call
@@ -9,14 +10,16 @@ export default defineEventHandler(async (event) => {
     
     const month = typeof query.month === 'string' ? query.month : null
 
-    const fileExists = fs.existsSync('data.json');
-    console.log(fileExists);
-    console.log(import.meta.url)
+    const fileExists = fs.existsSync('/data.json');
+    console.log('exists:',fileExists);
+    console.log('import.meta.url:', import.meta.url)
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.join(path.dirname(__filename));
     const __dirname_2 = path.join(path.dirname(__filename), '../..');
-    console.log(__dirname)
-    console.log(__dirname_2)
+    console.log('dirname', __dirname)
+    console.log('dirname_2:',__dirname_2)
+    console.log('cwd', process.cwd())
+    console.log('check:', fs.existsSync(path.join(process.cwd(), 'data.json')))
     // if (!fileExists) fs.writeFileSync('/data.json', JSON.stringify({}))
     
     const response = fs.readFileSync('data.json', { encoding: 'utf-8' })
